b0e5f1faaa5929fbc0d93fc9715fbd6f50fad46d
sahbaz23 <amdsahbaz19@gmail.com>
2025-10-29 22:18:59 +0530

    learn express

--- server/index.js
+++ server/index.js
@@ -1,32 +1,15 @@
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
-const myServer = http.createServer((req, res) => {
-  //   console.log(req);
-  if (req.url === "/favicon.ico") return res.end();
-  const log = `${Date.now()}:${req.url} New Request Received\n`;
-  const Myurl = url.parse(req.url, true);
-  console.log(Myurl);
+const express = require("express");
+const app = express();
 
-  fs.appendFile("./log.txt", log, (err, data) => {
-    switch (Myurl.pathname) {
-      case "/":
-        res.end("My Home Page");
-        break;
-      case "/about":
-        const userName = Myurl.query.my;
-        res.end(`Hello, ${userName}`);
-        break;
-      case "/search":
-        const search = Myurl.query.search_query;
-        res.end("Here is your result for " + search);
-        break;
-      default:
-        res.end("404 Not Found");
-    }
-  });
+app.get("/", (req, res) => {
+  res.send("My Home Page");
 });
+app.get("/about", (req, res) => {
+  const userName = "Sahbaz";
+  res.send(`Hello, ${userName}`);
+});
+
 const port = 8001;
-myServer.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
